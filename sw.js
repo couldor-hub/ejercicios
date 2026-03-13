6dee7ebaa68c7af65c73dec0ffea4f04b59f6267
couldor-hub <couldor@gmail.com>
2026-03-13 20:47:56 +0100

    debug sw cache

--- sw.js
+++ sw.js
@@ -5,25 +5,50 @@ var ARCHIVOS = [
   './style.css',
   './app.js',
   './musica.mp3',
-  './imagenes/Sentadillas.gif',
-  './imagenes/Sentadillas-sumo.gif',
-  './imagenes/Zancadas.gif',
-  './imagenes/Flexiones.gif',
-  './imagenes/Plancha.gif',
-  './imagenes/Crunch-Abdominal.gif',
-  './imagenes/Superman.gif',
-  './imagenes/Superman1.gif',
-  './imagenes/Press-hombros.gif',
-  './imagenes/Burpees.gif',
-  './imagenes/Jumping-Jacks.gif',
   './imagenes/icon-192.png',
-  './imagenes/icon-512.png'
+  './imagenes/icon-512.png',
+  './imagenes/Burpees.gif',
+  './imagenes/Butt-Kicks.jpg',
+  './imagenes/Jumping-Jacks.gif',
+  './imagenes/Mountain-Climbers.gif',
+  './imagenes/Rodillas-Altas.gif',
+  './imagenes/Saltos-Ninja.gif',
+  './imagenes/Sentadilla-con-Salto.gif',
+  './imagenes/Skater-Jumps.gif',
+  './imagenes/Tijeras-Saltando.gif',
+  './imagenes/Escaladores-Rapidos.gif',
+  './imagenes/Bicicleta-Abdominal.gif',
+  './imagenes/Tijeras-Abdominales.gif',
+  './imagenes/Elevacion-Piernas.gif',
+  './imagenes/Crunch-Giro.gif',
+  './imagenes/Plancha-Dinamica.gif',
+  './imagenes/Plancha-Lateral.gif',
+  './imagenes/Dead-Bug.gif',
+  './imagenes/Hollow-Hold.gif',
+  './imagenes/Rodillas-Pecho.gif',
+  './imagenes/Gato-Vaca.gif',
+  './imagenes/Postura-Nino.jpg',
+  './imagenes/Cobra.jpg',
+  './imagenes/Rotacion-Columna.jpg',
+  './imagenes/Puente-Gluteos.gif',
+  './imagenes/Superman.gif',
+  './imagenes/Estiramiento-Lumbar.jpg',
+  './imagenes/Crunch-Abdominal.gif',
+  './imagenes/Abdominales-Piernas.gif',
+  './imagenes/Abdominales-Rodilla.gif',
+  './imagenes/Abdominales-Tobillo.gif'
 ];
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(CACHE).then(function(cache) {
-      return cache.addAll(ARCHIVOS);
+      return Promise.all(
+        ARCHIVOS.map(function(url) {
+          return cache.add(url).catch(function(err) {
+            console.error('❌ Falla:', url);
+          });
+        })
+      );
     })
   );
   self.skipWaiting();
